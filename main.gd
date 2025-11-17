083b573366edf3153c2bd8d2d649ad9d6f3055b2
陈骏鑫 <chengjunxin@liankaa.com>
2025-11-17 22:59:18 +0800

    feat: 添加玩家、敌人、主场景

---
+++ main.gd
@@ -0,0 +1,25 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	pass # Replace with function body.
+
+func new_game() -> void:
+	score = 0;
+	$Player.start($StartPosition.position);
+	$StartTimer.start();
+	pass
